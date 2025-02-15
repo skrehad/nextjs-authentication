92f496108c81e5eb9681c5b4839be057069db3ec
rehadhasan <rehadhasan664@gmail.com>
2025-02-16 02:19:50 +0600

    user information update

--- src/app/(withDashboard)/dashboard/user-info/page.tsx
+++ src/app/(withDashboard)/dashboard/user-info/page.tsx
@@ -1,7 +1,7 @@
 const UserInfoPage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">User Info</h1>
+      <h1 className="text-4xl text-center mt-10">User Information</h1>
     </div>
   );
 };
